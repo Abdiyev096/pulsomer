cc130629ce78dc72aafba70425d025c720049d81
Shadiyar <luch.wells@mail.ru>
2020-04-01 01:38:41 +0600

    added js files and corusel

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left.png" alt="Стрелка"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right.png" alt="Стрелка"></button>'
+  });
+});
